ffe481afe69f3ec47fde9b424cbb5fecffa2894c
Nate Rudd <deafears@naterudd.com>
2017-02-05 16:42:29 +0900

    run.sample: moved photo_upload to the top of the run sample.  seems to be more appropriate

--- run.sample.sh
+++ run.sample.sh
@@ -3,10 +3,10 @@
 # When ready, uncomment the lines of the tools you wish to use,
 # you may want to uncomment the chown lines as well if desired.
 
+# php -f /full/path/to/flickr_toolset/photo_upload.php
+
 # php -f /full/path/to/flickr_toolset/desktop_download.php
 # chown -R user:group /full/path/to/flickr_toolset/desktops 
 
 # php -f /full/path/to/flickr_toolset/photo_download.php
 # chown -R user:group /full/path/to/flickr_toolset/download/photos 
-
-# php -f /full/path/to/flickr_toolset/photo_upload.php
